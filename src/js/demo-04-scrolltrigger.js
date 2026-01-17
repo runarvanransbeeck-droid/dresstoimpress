453aed4700432815e5b66b79454d87366b178744
vanransbeeck <runar.van.ransbeeck@student.howest.be>
2026-01-17 14:59:54 +0100

    lottiefiles basic setup and cleanup

---
+++ src/js/demo-04-scrolltrigger.js
@@ -0,0 +1,23 @@
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const player = new DotLottie({
+  canvas: document.querySelector("#anim"),
+  src: "assets/zipperanim2.json",
+});
+
+gsap.to(player.canvas, {
+  scrollTrigger: {
+    trigger: player.canvas,
+    start: 'top top',
+    end: '+=1000',
+    pin: true,
+    scrub: true,
+    onUpdate: (self) => {
+      player.setFrame(self.progress * (player.totalFrames-1));
+    }
+  },
+});
