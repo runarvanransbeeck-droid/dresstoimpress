a62a8fff41ba9d328173a52d1339d8a7eedd1e5c
vanransbeeck <runar.van.ransbeeck@student.howest.be>
2026-01-20 17:58:05 +0100

    added more elements + stretch interaction

---
+++ src/scripts/canvasstretch.js
@@ -0,0 +1,33 @@
+const initstretch = () => {
+    const frame = document.querySelector(".stretchcanvas");
+    const dragger = document.querySelector(".stretchcanvas-dragger");
+
+    let isDragging = false;
+
+    dragger.addEventListener("mousedown", () => {
+        isDragging = true;
+        document.body.style.cursor = "ns-resize";
+    });
+
+    document.addEventListener("mousemove", (e) => {
+        if (!isDragging) return;
+
+        const minHeight = 210;
+        const maxHeight = 800;
+
+        const newHeight = Math.min(
+            Math.max(e.clientY - frame.getBoundingClientRect().top, minHeight),
+            maxHeight
+        );
+
+        frame.style.height = `${newHeight}px`;
+    });
+
+    document.addEventListener("mouseup", () => {
+        isDragging = false;
+        document.body.style.cursor = "default";
+    });
+
+}
+
+initstretch();
