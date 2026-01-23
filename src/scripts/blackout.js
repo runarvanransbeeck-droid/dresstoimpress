5dbe530b49d04fcf9c0db0380395adce025023f0
vanransbeeck <runar.van.ransbeeck@student.howest.be>
2026-01-23 20:37:59 +0100

    final js mobile tweak

--- src/scripts/blackout.js
+++ src/scripts/blackout.js
@@ -67,7 +67,7 @@ gsap.matchMedia().add("(min-width: 600px)", () => {
     gsap.to(document.querySelector("#anim"), {
         scrollTrigger: {
             trigger: document.querySelector("#anim"),
-            start: 'top top-=300',
+            start: 'top top-=400',
             end: document.querySelector(".speakercontainer"),
             onEnter: () => {
                 gsap.to($body, {
