64d2599d9fc9c7d2f65fba2857a8939d9acdcf70
vanransbeeck <runar.van.ransbeeck@student.howest.be>
2026-01-24 18:11:54 +0100

    stretch canvas on all breakpoints

--- src/scripts/canvasstretch.js
+++ src/scripts/canvasstretch.js
@@ -1,37 +1,114 @@
+import gsap from 'gsap';
+
 const initstretch = () => {
     const frame = document.querySelector(".stretchcanvas");
     const dragger = document.querySelector(".stretchcanvas__dragger");
 
     let isDragging = false;
 
-    dragger.addEventListener("pointerdown", (e) => {
-        console.log("mousedown");
-        isDragging = true;
-        dragger.setPointerCapture?.(e.pointerId);
-        document.body.style.cursor = "ns-resize";
+    gsap.matchMedia().add("(max-width: 599px)", () => {
+        
+
+        frame.style.height = "210px";
+        frame.style.width = "235px";
+        
+        dragger.addEventListener("pointerdown", (e) => {
+            console.log("mousedown");
+            isDragging = true;
+            dragger.setPointerCapture?.(e.pointerId);
+            document.body.style.cursor = "ns-resize";
+        });
+
+        document.addEventListener("pointermove", (e) => {
+            if (!isDragging) return;
+
+            const minHeight = 210;
+            const maxHeight = 800;
+
+            const newHeight = Math.min(
+                Math.max(e.clientY - frame.getBoundingClientRect().top, minHeight),
+                maxHeight
+            );
+
+            frame.style.height = `${newHeight}px`;
+        });
+
+        const stopDragging = () => {
+            isDragging = false;
+            document.body.style.cursor = "default";
+        };
+
+        window.addEventListener("pointerup", stopDragging);
+        window.addEventListener("pointercancel", stopDragging);
     });
 
-    document.addEventListener("pointermove", (e) => {
-        if (!isDragging) return;
+    gsap.matchMedia().add("(min-width: 600px)", () => {
 
-        const minHeight = 210;
-        const maxHeight = 700;
+        frame.style.height = "210px";
+        frame.style.width = "235px";
+        dragger.addEventListener("pointerdown", (e) => {
+            console.log("mousedown");
+            isDragging = true;
+            dragger.setPointerCapture?.(e.pointerId);
+            document.body.style.cursor = "ns-resize";
+        });
 
-        const newHeight = Math.min(
-            Math.max(e.clientY - frame.getBoundingClientRect().top, minHeight),
-            maxHeight
-        );
+        document.addEventListener("pointermove", (e) => {
+            if (!isDragging) return;
 
-        frame.style.height = `${newHeight}px`;
+            const minHeight = 210;
+            const maxHeight = 900;
+
+            const newHeight = Math.min(
+                Math.max(e.clientY - frame.getBoundingClientRect().top, minHeight),
+                maxHeight
+            );
+
+            frame.style.height = `${newHeight}px`;
+        });
+
+        const stopDragging = () => {
+            isDragging = false;
+            document.body.style.cursor = "default";
+        };
+
+        window.addEventListener("pointerup", stopDragging);
+        window.addEventListener("pointercancel", stopDragging);
     });
 
-    const stopDragging = () => {
-        isDragging = false;
-        document.body.style.cursor = "default";
-    };
+    gsap.matchMedia().add("(min-width: 1024px)", () => {
+        
+        frame.style.height = "18rem";
+        frame.style.width = "220px";
+        dragger.addEventListener("pointerdown", (e) => {
+            console.log("mousedown");
+            isDragging = true;
+            dragger.setPointerCapture?.(e.pointerId);
+            document.body.style.cursor = "ew-resize";
+        });
 
-    window.addEventListener("pointerup", stopDragging);
-    window.addEventListener("pointercancel", stopDragging);
+        document.addEventListener("pointermove", (e) => {
+            if (!isDragging) return;
+
+            const minWidth = 183;
+            const maxWidth = 800;
+
+            const newWidth = Math.min(
+                Math.max(e.clientX - frame.getBoundingClientRect().left, minWidth),
+                maxWidth
+            );
+
+            frame.style.width = `${newWidth}px`;
+        });
+
+        const stopDragging = () => {
+            isDragging = false;
+            document.body.style.cursor = "default";
+        };
+
+        window.addEventListener("pointerup", stopDragging);
+        window.addEventListener("pointercancel", stopDragging);
+    });
 }
 
 initstretch();
