2489317d0253b3e7b23791defa8a5436d723c07a
vanransbeeck <runar.van.ransbeeck@student.howest.be>
2026-01-25 13:10:53 +0100

    new style, caroussel, outro, nav for desktop size +  button hovers

--- src/scripts/caroussel.js
+++ src/scripts/caroussel.js
@@ -1,3 +1,4 @@
+import gsap from 'gsap';
 const loop = document.querySelector('.carousselloop');
 const loop1 = document.querySelector('.carousselloop1');
 const loop2 = document.querySelector('.carousselloop2');
@@ -5,42 +6,82 @@ const prevbutton = document.querySelector('.caroussel__prevbutton');
 const nextbutton = document.querySelector('.caroussel__nextbutton');
 
 const initcaroussel = () => {
-    let page = 1;
-    let isScrolling = false;
+    gsap.matchMedia().add("(max-width: 1299px)", () => {
+        let page = 1;
+        let isScrolling = false;
 
-    const handleScroll = (direction) => {
-        if (isScrolling) return;
-        isScrolling = true;
+        const handleScroll = (direction) => {
+            if (isScrolling) return;
+            isScrolling = true;
 
-        loop.scrollBy({
-            left: direction === 'next' ? 200 : -200,
-            behavior: 'smooth'
-        });
+            loop.scrollBy({
+                left: direction === 'next' ? 200 : -200,
+                behavior: 'smooth'
+            });
 
-        setTimeout(() => {
-            if (direction === 'next') {
-                if (page === 6) {
-                    page = 1;
-                    loop1.style.order = 2;
-                    loop.scrollLeft = 0;
+            setTimeout(() => {
+                if (direction === 'next') {
+                    if (page === 6) {
+                        page = 1;
+                        loop1.style.order = 2;
+                        loop.scrollLeft = 0;
+                    } else {
+                        page++;
+                    }
                 } else {
-                    page++;
+                    if (page === 1) {
+                        page = 6;
+                        loop2.style.order = 1;
+                        loop.scrollLeft = loop.scrollWidth - (loop.clientWidth + 40);
+                    } else {
+                        page--;
+                    }
                 }
-            } else {
-                if (page === 1) {
-                    page = 6;
-                    loop2.style.order = 1;
-                    loop.scrollLeft = loop.scrollWidth - (loop.clientWidth + 40);
-                } else {
-                    page--;
-                }
-            }
-            isScrolling = false;
-        }, 400);
-    };
+                isScrolling = false;
+            }, 400);
+        };
 
-    prevbutton.addEventListener('pointerdown', () => handleScroll('prev'));
-    nextbutton.addEventListener('pointerdown', () => handleScroll('next'));
+        prevbutton.addEventListener('pointerdown', () => handleScroll('prev'));
+        nextbutton.addEventListener('pointerdown', () => handleScroll('next'));
+    });
+    gsap.matchMedia().add("(min-width: 1300px)", () => {
+        let page = 1;
+        let isScrolling = false;
+
+        const handleScroll = (direction) => {
+            if (isScrolling) return;
+            isScrolling = true;
+
+            loop.scrollBy({
+                left: direction === 'next' ? 333 : -333,
+                behavior: 'smooth'
+            });
+
+            setTimeout(() => {
+                if (direction === 'next') {
+                    if (page === 6) {
+                        page = 1;
+                        loop1.style.order = 2;
+                        loop.scrollLeft = 0;
+                    } else {
+                        page++;
+                    }
+                } else {
+                    if (page === 1) {
+                        page = 6;
+                        loop2.style.order = 1;
+                        loop.scrollLeft = loop.scrollWidth - (loop.clientWidth + 40);
+                    } else {
+                        page--;
+                    }
+                }
+                isScrolling = false;
+            }, 400);
+        };
+
+        prevbutton.addEventListener('pointerdown', () => handleScroll('prev'));
+        nextbutton.addEventListener('pointerdown', () => handleScroll('next'));
+    });
 };
 
 initcaroussel();
